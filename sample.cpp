491c198f493603ec1a3c6ab1d516d07e921b1dbb
EhrenDavies1 <ehren.davies@student.northampton.edu>
2025-08-29 01:42:01 +0000

    Completed Assignment

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,20 @@
-// YOUR NAME HERE
+// Ehren Davies
+
 #include <iostream>
 using namespace std;
 
-int main () {
+int main() {
+    int num1, num2, sum;
 
+    cout << "Enter first integer: ";
+    cin >> num1;
+
+    cout << "Enter second integer: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "\nThe sum is " << sum << "." << endl;
+
+    return 0;
 }
